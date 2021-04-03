e99aa57ba0f1085560cd6d229334d7a1b0e6071f
ariju <jun.0309ari@gmail.com>
2021-04-03 17:33:46 +0900

    サインイン機能の実装

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,12 +4,38 @@
       <h1>whisp.</h1>
     </router-link>
     <div class="btns">
-      <button>
+      <button @click="signIn">
         <fa icon="user" />
       </button>
     </div>
   </header>
 </template>
+
+<script>
+import firebase from "firebase"
+import {auth} from "../main"
+
+export default {
+  methods: {
+    signIn() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      auth.signInWithPopup(provider)
+      .then((result) => {
+        alert("Hello,"+result.user.displayName+"!")
+      })
+    },
+    signOut() {
+      if(window.confirm('Are You Sure to Sign Out?'))//メッセージとokとキャンセルボタン
+        auth.signOut()
+        .then(() => {
+          alert("You Safely Signed Out.")
+          this.$router.push('/'),
+          location.reload()//ページを再読み込み
+        })
+    }
+  }
+}
+</script>
 
 <style lang="stylus" scoped>
 header
