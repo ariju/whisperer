130aa3180e81e23f5c92733b91840540032b8663
ariju <jun.0309ari@gmail.com>
2021-04-05 22:54:14 +0900

    ユーザーが投稿したwhisperのリスト表示

--- src/views/User.vue
+++ src/views/User.vue
@@ -4,25 +4,44 @@
       <div class="avatar" :style="'background-image: url('+user.photoURL+')'"></div>
       <div class="texts">
         <h1>{{user.name}}</h1>
+        <p v-if="myWhispers.length > 1">{{myWhispers.length}} whispers</p>
+        <p v-else>{{myWhispers.length}} whisper</p>
       </div>
+    </div>
+    <div class="list">
+      <Item
+      v-for="whisper in orderBy(myWhispers,'date',-1)"
+      :key="whisper.id"
+      :id="whisper.id"
+      :uid="whisper.uid"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import { db } from '../main'
+import firebase from 'firebase'
+import Item from '@/components/Item'
+import Vue2Filters from 'vue2-filters'
 
 export default {
+  components: {
+    Item
+  },
   data () {
     return {
-      user: {}
+      user: {},
+      myWhispers: []
     }
   },
   firestore () {
     return {
-      user: db.collection('users').doc(this.$route.params.uid)
+      user: db.collection('users').doc(this.$route.params.uid),
+      myWhispers: db.collection('whispers').where('uid','==',this.$route.params.uid)
     }
-  }
+  },
+  mixins:[Vue2Filters.mixin]
 }
 </script>
 
